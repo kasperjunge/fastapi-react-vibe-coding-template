132bc582b8092382671bf113eb199567c2720ee3
Kasper Junge <kasperjuunge@gmail.com>
2025-05-04 21:56:13 +0200

    .

--- app/backend/src/backend/database.py
+++
@@ -1,8 +0,0 @@
-from sqlmodel import create_engine, SQLModel
-from backend.settings import settings
-
-engine = create_engine(settings.DATABASE_URL)
-
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
-    
--- app/backend/src/backend/main.py
+++ app/backend/src/backend/main.py
@@ -1,9 +1,18 @@
 import uvicorn
 from backend.api import app
+from backend.settings import settings
 
 def main() -> None:
     print("Hello from backend!!")
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+    reload = settings.ENVIRONMENT == "local"
+
+    uvicorn.run(
+        "backend.main:app", 
+        host="0.0.0.0", 
+        port=8000, 
+        reload=reload
+    )
 
 if __name__ == "__main__":
     main()
