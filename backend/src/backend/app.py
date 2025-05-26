3a9797484e8260897fc055312fd3cc50ed1b98e2
Kasper Junge <kasperjuunge@gmail.com>
2025-05-26 22:30:45 +0200

    user model migrated + example service commmented out

--- backend/src/backend/app.py
+++ backend/src/backend/app.py
@@ -2,7 +2,6 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
-from backend.services.example_service.routes import router as example_router
 from backend.db import create_db_and_tables
 from backend.services.auth.routes import router as auth_router
 from backend.services.users.routes import router as users_router
@@ -16,7 +15,6 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-app.include_router(example_router, tags=["example"])
 app.include_router(auth_router, tags=["auth"])
 app.include_router(users_router, tags=["users"])
 
